9c6afbd8ec7e8a23d168ed56bb51aec73f145c51
John Palgut <jwsonic@gmail.com>
2015-10-01 22:42:19 -0500

    debugging

--- condump.js
+++ condump.js
@@ -1,63 +1,69 @@
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
-import {Promise} from 'es6-promise';
+import {
+  Promise
+}
+from 'es6-promise';
 
-const gameInit = 'C:Gamerules: entering state \'DOTA_GAMERULES_STATE_INIT\'';
+const gameInit = `C:Gamerules: entering state 'DOTA_GAMERULES_STATE_INIT`;
 const rePlayerTeam = /Game event "player_team", Tick \d+:\r\n- "userid" = "(\d+)"/;
 
-export default class Condump {
 
-  /**
-    Takes a console dump file name, reads in the data, and processes it for hero pick information
-    @param {string} fName - The console dump file to open
-    @return (Promise) - A promise that will resolve the name of the hero that was picked
-  */
-  findHeroInFile(fName) {
-    return new Promise((resolve, reject) => {
-      try {
-        let data = fs.readFileSync(fName, 'utf8');
-        let heroName = findHero(data);
+/**
+  Takes a console dump file name, reads in the data, and processes it for hero pick information
+  @param {string} fName - The console dump file to open
+  @return (Promise) - A promise that will resolve the name of the hero that was picked
+*/
+export function findHeroInFile(fName) {
+  return new Promise((resolve, reject) => {
+    try {
+      let data = fs.readFileSync(fName, 'utf8');
+      let heroName = findHero(data);
 
-        if (heroName) {
-          resolve(heroName);
-        } else {
-          reject('No hero selection found!');
-        }
-      } catch (e) {
-        reject(e);
+      if (heroName) {
+        resolve(heroName);
+      } else {
+        reject('No hero selection found!');
       }
-    });
-  }
-
-  /**
-    Takes console dump file data and processes it for hero pick information
-    @param {string} conDumpData - String data read from a condump file
-    @return (string) - A string that is the hero name if it's found or null otherwise
-  */
-  findHero(conDumpData) {
-    //If the condump contains multiple games, make sure we get the most recent one
-    let parts = conDumpData.split(gameInit);
-    let gameData = parts[parts.length - 1];
-
-    //Look for the userId
-    let userIds = rePlayerTeam.exec(gameData);
-
-    if (userIds) {
-      //player_team events for players connecting before you don't seem to show up, therefore the first player id match is your id
-      let userId = userIds[1];
-
-      //Build a regex specifically for our userId
-      let reHeroPick = new RegExp(`Game event "dota_player_pick_hero", Tick \\d+:\r\n- "player" = "${userId}"\r\n- "heroindex" = "\\d+"\r\n- "hero" = "npc_dota_hero_([a-z_]+)"`);
-
-      //Look for the hero picks
-      let heroPick = reHeroPick.exec(gameData);
-
-      if (heroPick) {
-        return heroPick[1];
-      }
-
-      return null;
+    } catch (e) {
+      reject(e);
     }
+  });
+}
+
+/**
+  Takes console dump file data and processes it for hero pick information
+  @param {string} conDumpData - String data read from a condump file
+  @return (string) - A string that is the hero name if it's found or null otherwise
+*/
+export function findHero(conDumpData) {
+  //If the condump contains multiple games, make sure we get the most recent one
+  let parts = conDumpData.split(gameInit);
+
+  console.log(`Parts: ${parts.length}`);
+
+  let gameData = parts[parts.length - 1];
+
+  //Look for the userId
+  let userIds = rePlayerTeam.exec(gameData);
+
+  if (userIds) {
+    //player_team events for players connecting before you don't seem to show up, therefore the first player id match is your id
+    let userId = userIds[1];
+
+    console.log(userId);
+
+    //Build a regex specifically for our userId
+    let reHeroPick = new RegExp(`Game event "dota_player_pick_hero", Tick \\d+:\r\n- "player" = "${userId}"\r\n- "heroindex" = "\\d+"\r\n- "hero" = "npc_dota_hero_([a-z_]+)"`);
+
+    //Look for the hero picks
+    let heroPick = reHeroPick.exec(gameData);
+
+    if (heroPick) {
+      return heroPick[1];
+    }
+
+    return null;
   }
 }
--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 import {findHeroInFile} from './condump.js';
 import chokidar from 'chokidar';
+import path from 'path';
+import fs from 'fs';
 
-let watcher = chokidar.watch();
+const dotaPath = path.resolve('/', 'Program Files (x86)', 'Steam', 'steamapps', 'common', 'dota 2 beta', 'game', 'dota');
+const reCondumpFile = /condump\d\d\d\.txt$/;
 
-watcher.on('add', path => {
-  findHeroInFile(path)
-    .then(heroName => console.log(`You picked: ${heroName}!`))
-    .catch(err => console.error(err));
+fs.watch(dotaPath, (event, fPath) => {
+  if (event === 'rename' && reCondumpFile.test(fPath)) {
+    console.log(`Processing: ${fPath}`);
+    findHeroInFile(path.join(dotaPath, fPath))
+      .then(heroName => console.log(`You picked: ${heroName}!`))
+      .catch(err => console.error(err));
+  }
 });
 
 console.log('Now watching for heroes!');
